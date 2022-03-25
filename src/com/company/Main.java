06358ac072d1d0163d65dbc1022d964b8b3da1ad
Asanbai Ortosaev <asanbaiortosaev@gmail.com>
2022-03-25 21:18:24 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(RomanToArabic.covertRomanToArabic("II"));
+
+        System.out.println(ArabicToRoman.convertArabicToRoman(9));
+
+
+
+
+    }
+}
+
